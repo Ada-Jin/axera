d0a6836d5e5419f6dfc7d9a84ff1fa8ad6a3b894
Ada-Jin <63859900+Ada-Jin@users.noreply.github.com>
2021-08-09 15:42:48 +0800

    Add files via upload

---
+++ ax_isp_iq_api - 副本.h
@@ -0,0 +1,36 @@
+/**********************************************************************************
+ *
+ * Copyright (c) 2019-2020 Beijing AXera Technology Co., Ltd. All Rights Reserved.
+ *
+ * This source file is the property of Beijing AXera Technology Co., Ltd. and
+ * may not be copied or distributed in any isomorphic form without the prior
+ * written consent of Beijing AXera Technology Co., Ltd.
+ *
+ * Auther: ZhiJun Zhou
+ * Data: 2021-06-05
+ * Mail: zhijunzhou@axera-tech.com
+ *
+ **********************************************************************************/
+
+#ifndef __AX_ISP_IQ_API_H__
+#define __AX_ISP_IQ_API_H__
+
+#include "ax_base_type.h"
+#include "ax_global_type.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define AX_ISP_AUTO_TABLE_MAX_NUM           (16)
+#define AX_ISP_GAIN_GRP_NUM                 (16)
+#define AX_ISP_EXPOSE_TIME_GRP_NUM          (10)
+
+#define AX_ISP_BLC_SBL_VAL_NUM              (16)
+#define AX_ISP_BLC_SBL_WIN_NUM              (4)
+
+#define AX_ISP_BLC_MESH_SIZE_H              (24)
+#define AX_ISP_BLC_MESH_SIZE_V              (24)
+
+#define AX_ISP_BLC_GBL_IIR_SIZE             (8)
